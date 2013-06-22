3f648e9082ae143c753b8b6552c7f8f600f7d8cb
akun <victorsos@163.com>
2013-06-22 09:02:34 +0800

    使用说明和路径变更

---
+++ README.md
@@ -0,0 +1,40 @@
+Pylint Plugin
+=============
+
+A plugin make Pylint more Django-Friendly.
+
+NOTICE: Django 1.4+ has solved the i18n problem like "_ is not callable".
+
+install
+-------
+
+python setup.py install
+
+usage
+-----
+
+1. save as example.py
+
+::
+
+   from django.utils.translation import ugettext_lazy as _
+
+   print _('damn it!')
+
+2. run pure pylint
+
+$ pylint -E example.py
+
+::
+
+   No config file found, using default configuration
+   ************* Module example
+   E:  3,6: _ is not callable
+
+3. run pylint with plugin
+
+pylint -E --load-plugins=PylintPlugin.astng_django example.py
+
+::
+
+   No config file found, using default configuration
--- src/astng_django.py
+++ src/astng_django.py
@@ -1,19 +1,21 @@
 from logilab.astng import MANAGER
 from logilab.astng.builder import ASTNGBuilder
 
-def hashlib_transform(module):
-    if module.name == 'django.utils.translation':
-        fake = ASTNGBuilder(MANAGER).string_build('''
+def fake_i18n(module):
 
+    if module.name == 'django.utils.translation':
+
+        fake = ASTNGBuilder(MANAGER).string_build('''
 def ugettext_lazy(value):
     return u''
 
 def ugettext(value):
     return u''
+'''
+        )
 
-''')
         for hashfunc in ('ugettext_lazy', 'ugettext'):
             module.locals[hashfunc] = fake.locals[hashfunc]
 
 def register(linter):
-    MANAGER.register_transformer(hashlib_transform)
+    MANAGER.register_transformer(fake_i18n)
